4c57f30a096fd1e38794554786bbec477635a8fc
Eugene Oh Yun Zheng <eugeneoyz.12.03.n@gmail.com>
2025-08-20 09:06:49 +0800

    Create Task.java

---
+++ src/main/java/Task.java
@@ -0,0 +1,24 @@
+public class Task {
+  String name;
+  boolean isComplete;
+
+  public Task(String name, boolean isComplete) {
+    this.name = name;
+    this.isComplete = isComplete;
+  }
+
+  public Task(String name) {
+    this(name, false);
+  }
+  
+  public void complete() { this.isComplete = true; }
+  public void uncomplete() { this.isComplete = false; }
+
+  @Override
+  public String toString() {
+      return "[%s] %s".formatted(
+        this.isComplete ? "X" : " ",
+        this.name
+      );
+  }
+}
