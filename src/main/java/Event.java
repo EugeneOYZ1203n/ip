d29ac52217c10801f82474d452eeb4a43e4468e3
Eugene Oh Yun Zheng <eugeneoyz.12.03.n@gmail.com>
2025-08-20 09:27:05 +0800

    Create Event.java

---
+++ src/main/java/Event.java
@@ -0,0 +1,18 @@
+public class Event extends Task {
+  String fromTime;
+  String toTime;
+
+  public Event(String name, boolean isComplete, String fromTime, String toTime) {
+    super(name, isComplete);
+    this.fromTime = fromTime;
+    this.toTime = toTime;
+  }
+  public Event(String name, String fromTime, String toTime) {
+    this(name, false, fromTime, toTime);
+  }
+
+  @Override
+  public String toString() {
+      return "[D]%s (from: %s to: %s)".formatted(super.toString(), this.fromTime, this.toTime);
+  }
+}
