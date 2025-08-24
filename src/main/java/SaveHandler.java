2940eac05acce5a4d58778e95101eeea54d89c33
Eugene Oh Yun Zheng <eugeneoyz.12.03.n@gmail.com>
2025-08-24 18:48:33 +0800

    Create SaveHandler.java

---
+++ src/main/java/SaveHandler.java
@@ -0,0 +1,38 @@
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class SaveHandler {
+  private final Path savePath;
+
+  public SaveHandler(String savePathName) {
+    this.savePath = Paths.get(savePathName);
+  }
+
+  public void save(String[] saveStrings) throws IOException {
+    if (!Files.exists(savePath)) { 
+      Files.createDirectories(savePath.getParent());
+    }
+    try (BufferedWriter writer = Files.newBufferedWriter(savePath)) {
+        for (String line : saveStrings) {
+            writer.write(line);
+            writer.newLine();
+        } 
+    }
+  }
+
+  public String[] load() throws IOException {
+    if (!Files.exists(savePath)) {
+      Files.createDirectories(savePath.getParent());
+      return new String[0];
+    }
+
+    List<String> lines = Files.readAllLines(savePath);
+
+    return (String[]) lines.toArray();
+  }
+}
