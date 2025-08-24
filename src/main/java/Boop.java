ee32b7407e3210e07bb9947d9baefd29a08e3b13
Eugene Oh Yun Zheng <eugeneoyz.12.03.n@gmail.com>
2025-08-24 19:15:26 +0800

    Format switch and case code

--- src/main/java/Boop.java
+++ src/main/java/Boop.java
@@ -20,43 +20,43 @@ public class Boop {
 
             try {
                 switch (command) {
-                    case "bye" -> {
-                        printSection(Farewell.farewell());
-                        break OUTER;
-                    }
-                    case "mark" -> {
-                        printSection(TaskList.mark(getIndexArgument(words)));
-                    }
-                    case "unmark" -> {
-                        printSection(TaskList.unmark(getIndexArgument(words)));
-                    }
-                    case "delete" -> {
-                        printSection(TaskList.deleteTask(getIndexArgument(words)));
-                    }
-                    case "list" -> printSection(TaskList.display());
-                    case "todo" -> {
-                        if (words.length < 2) { throw new BoopError("Ya missing da name!"); }
-                        printSection(TaskList.addToList(new Todo(words[1])));
-                    }
-                    case "deadline" -> {
-                        if (words.length < 2) { throw new BoopError("Ya missing da name!"); }
-                        String[] parts = words[1].split("/by", 2);
-                        if (parts.length < 2) { throw new BoopError("Ya missing da deadline!"); }
-                        String name = parts[0].trim();
-                        String by = parts[1].trim();
-                        printSection(TaskList.addToList(new Deadline(name, by)));
-                    }
-                    case "event" -> {
-                        if (words.length < 2) { throw new BoopError("Ya missing da name!"); }
-                        String[] parts = words[1].split("/from|/to");
-                        if (parts.length < 2) { throw new BoopError("Ya missing da start time!"); }
-                        if (parts.length < 3) { throw new BoopError("Ya missing da end time!"); }
-                        String name = parts[0].trim();
-                        String from = parts[1].trim();
-                        String to = parts[2].trim();
-                        printSection(TaskList.addToList(new Event(name, from, to)));
-                    }
-                    default -> throw new BoopError("Don't get wut ya sayin missy. Say it again!");
+                case "bye" -> {
+                    printSection(Farewell.farewell());
+                    break OUTER;
+                }
+                case "mark" -> {
+                    printSection(TaskList.mark(getIndexArgument(words)));
+                }
+                case "unmark" -> {
+                    printSection(TaskList.unmark(getIndexArgument(words)));
+                }
+                case "delete" -> {
+                    printSection(TaskList.deleteTask(getIndexArgument(words)));
+                }
+                case "list" -> printSection(TaskList.display());
+                case "todo" -> {
+                    if (words.length < 2) { throw new BoopError("Ya missing da name!"); }
+                    printSection(TaskList.addToList(new Todo(words[1])));
+                }
+                case "deadline" -> {
+                    if (words.length < 2) { throw new BoopError("Ya missing da name!"); }
+                    String[] parts = words[1].split("/by", 2);
+                    if (parts.length < 2) { throw new BoopError("Ya missing da deadline!"); }
+                    String name = parts[0].trim();
+                    String by = parts[1].trim();
+                    printSection(TaskList.addToList(new Deadline(name, by)));
+                }
+                case "event" -> {
+                    if (words.length < 2) { throw new BoopError("Ya missing da name!"); }
+                    String[] parts = words[1].split("/from|/to");
+                    if (parts.length < 2) { throw new BoopError("Ya missing da start time!"); }
+                    if (parts.length < 3) { throw new BoopError("Ya missing da end time!"); }
+                    String name = parts[0].trim();
+                    String from = parts[1].trim();
+                    String to = parts[2].trim();
+                    printSection(TaskList.addToList(new Event(name, from, to)));
+                }
+                default -> throw new BoopError("Don't get wut ya sayin missy. Say it again!");
                 }
             } catch (BoopError e) {
                 printSection(ErrorHandler.getErrorMessage(e));
