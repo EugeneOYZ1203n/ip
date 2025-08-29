24eff5371d5c0a7710f24f7c536082cb4ee24011
Eugene Oh Yun Zheng <eugeneoyz.12.03.n@gmail.com>
2025-08-29 17:01:08 +0800

    Create SaveHandlerStub.java

---
+++ src/test/java/app/SaveHandlerStub.java
@@ -0,0 +1,27 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import app.SaveHandler;
+
+public class SaveHandlerStub extends SaveHandler {
+
+    List<String> saveData;
+
+    public SaveHandlerStub() {
+        super("");
+        saveData = new ArrayList<>();
+    }
+
+    @Override
+    public void save(String[] saveStrings) {
+        saveData.addAll(Arrays.asList(saveStrings));
+    }
+
+    @Override
+    public String[] load() {
+        return saveData.toArray(String[]::new);
+    }
+}
