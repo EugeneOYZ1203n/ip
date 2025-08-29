9c365f377137bb89e7a3d43e1f1cc80eecb7ebf1
Eugene Oh Yun Zheng <eugeneoyz.12.03.n@gmail.com>
2025-08-29 16:59:52 +0800

    Update classes to facilitate testing

--- src/main/java/app/SaveHandler.java
+++ src/main/java/app/SaveHandler.java
@@ -7,7 +7,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-public final class SaveHandler {
+public class SaveHandler {
   private final Path savePath;
 
   public SaveHandler(String savePathName) {
